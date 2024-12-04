da14e4345f0b558b3ad5c3ea16d1c15c2104f9e5
jinho-yoo-jack <jhy7342@gmail.com>
2024-12-04 22:19:46 +0900

    SOLVED-1: FindTheWay

--- src/main/java/org/coding/test/binary_search/FindingTheWay.java
+++ src/main/java/org/coding/test/binary_search/FindingTheWay.java
@@ -1,6 +1,7 @@
 package org.coding.test.binary_search;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.IntStream;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/42892?language=java
 public class FindingTheWay {
@@ -46,32 +47,81 @@ public class FindingTheWay {
         public Node getRight() {
             return right;
         }
-
     }
+
+    public static void insert(Node parent, Node child) {
+        if (child.getX() < parent.getX()) {
+            if (parent.getLeft() == null) {
+                parent.setLeft(child);
+            } else {
+                insert(parent.getLeft(), child);
+            }
+        } else {
+            if (parent.getRight() == null) {
+                parent.setRight(child);
+            } else {
+                insert(parent.getRight(), child);
+            }
+        }
+    }
+
+    public static void preOrder2(Node searchingNode, List<Integer> searchedNodes) {
+        if (searchingNode != null) {
+            searchedNodes.add(searchingNode.index);
+            preOrder2(searchingNode.left, searchedNodes);
+            preOrder2(searchingNode.right, searchedNodes);
+        }
+    }
+
+    public static void postOrder(Node searchingNode, List<Integer> searchedNodes) {
+        if (searchingNode != null) {
+            postOrder(searchingNode.left, searchedNodes);
+            postOrder(searchingNode.right, searchedNodes);
+            searchedNodes.add(searchingNode.index);
+        }
+    }
+
+
+
+
+
     public static void main(String[] args) {
         int[][] nodeinfo = new int[][]{
-                {5, 3}, // 0
-                {11, 5},// 1
-                {13, 3},// 2
-                {3, 5}, // 3
-                {6, 1}, // 4
-                {1, 3}, // 5
-                {8, 6}, // 6
-                {7, 2}, // 7
-                {2, 2}}; // 8
+            {5, 3}, // 0
+            {11, 5},// 1
+            {13, 3},// 2
+            {3, 5}, // 3
+            {6, 1}, // 4
+            {1, 3}, // 5
+            {8, 6}, // 6
+            {7, 2}, // 7
+            {2, 2}}; // 8
 
-        int data_size = nodeinfo.length;
+        PriorityQueue<Node> nodes = new PriorityQueue<>((o1, o2) -> {
+            if (o1.y == o2.y) return o1.x - o2.x;
+            else return o2.y - o1.y;
+        });
+        IntStream.range(0, nodeinfo.length)
+            .forEach(i -> {
+                nodes.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
+            });
 
-        int[][] sequencedData = new int[data_size][3];
-        for (int i = 0; i < data_size; i++) {
-            sequencedData[i][0] = nodeinfo[i][0];
-            sequencedData[i][1] = nodeinfo[i][1];
-            sequencedData[i][2] = i + 1;
+        nodes.forEach(node -> {
+            System.out.println("x:" + node.x + ", y:" + node.y);
+        });
+
+        Node root = nodes.poll();
+        while (!nodes.isEmpty()) {
+            Objects.requireNonNull(root);
+            insert(root, nodes.poll());
         }
 
-        Arrays.sort(sequencedData, (o1, o2) -> {
-            if(o1[1] == o2[1]) return o1[0] - o2[0]; // y좌표 값이 동일한 경우, x좌표 기준 ASC 정렬
-            else return o2[1] - o1[1]; // y좌표 기준 DESC 정렬
-        });
+        List<Integer> searchedNodes = new ArrayList<>();
+        preOrder2(root, searchedNodes);
+        List<Integer> postSearchedNodes = new ArrayList<>();
+        postOrder(root, postSearchedNodes);
+        searchedNodes.forEach(node -> System.out.print(node + ","));
+
+
     }
 }
